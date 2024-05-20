c44a5d0fc399641e5fb191f0b798f50546b2454a
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-05-19 21:54:10 -0400

    Quitar el Default en las variables de Security Groups

--- modules/3-security-groups/variables.tf
+++ modules/3-security-groups/variables.tf
@@ -1,35 +1,42 @@
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
 variable "ssh_port" {
-  description = "Port for SSH"
+  description = "Port for SSH access"
   type        = number
-  default     = 22
 }
 
 variable "http_port" {
-  description = "Port for HTTP"
+  description = "Port for HTTP access"
   type        = number
-  default     = 80
 }
 
 variable "https_port" {
-  description = "Port for HTTPS"
+  description = "Port for HTTPS access"
   type        = number
-  default     = 443
 }
 
 # Variable para la dirección IP permitida para acceder por SSH a la instancia EC2
 variable "ssh_ip" {
-  description = "IP address allowed to SSH into the EC2 instance"  # Descripción de la variable
-  default     = "192.168.0.11/32"                                  # Valor por defecto para la dirección IP permitida
+  description = "IP address allowed to SSH into the EC2 instance"
+  type        = string
 }
 
 # Variable para el rango de direcciones IP del servicio Conexión de instancias de EC2
 variable "ec2_connect_ips" {
-  description = "IP range for EC2 Instance Connect service"  # Descripción de la variable
-  default     = "18.206.107.24/29"                           # Valor por defecto para el rango de direcciones IP
+  description = "IP range for EC2 Instance Connect service"
+  type        = string
 }
 
 # Variable para el CIDR block que permite todo el tráfico
 variable "all_traffic_cidr" {
-  description = "CIDR block to allow all traffic"  # Descripción de la variable
-  default     = "0.0.0.0/0"                        # Valor por defecto para permitir todo el tráfico
+  description = "CIDR block to allow all traffic"
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the Security Group"
+  type        = string
 }

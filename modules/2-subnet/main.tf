2bc49db8f81976547c7ab0a0258498a4810b5894
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-05-19 21:48:55 -0400

    usar la variable id en el modulo Subnet

--- modules/2-subnet/main.tf
+++ modules/2-subnet/main.tf
@@ -1,6 +1,6 @@
 # Crear una subred pública
 resource "aws_subnet" "free_tier_subnet" {
-  vpc_id            = aws_vpc.free_tier_vpc.id
+  vpc_id            = var.vpc_id
   cidr_block        = var.subnet_cidr
   availability_zone = var.availability_zone
 

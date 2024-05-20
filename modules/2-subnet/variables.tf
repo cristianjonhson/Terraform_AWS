f65dc6b7fa8e5ad48042de70770658ed42870b68
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-05-19 21:48:20 -0400

    Quitar el Default en las variables del Subnet

--- modules/2-subnet/variables.tf
+++ modules/2-subnet/variables.tf
@@ -6,13 +6,11 @@ variable "vpc_id" {
 # Variable para el CIDR block de la subred pública
 variable "subnet_cidr" {
   description = "CIDR block for the public subnet"  # Descripción de la variable
-  default     = "10.0.1.0/24"                       # Valor por defecto para el CIDR block de la subred
 }
 
 # Variable para la zona de disponibilidad de la subred
 variable "availability_zone" {
   description = "Availability zone for the subnet"  # Descripción de la variable
-  default     = "us-east-1a"                        # Valor por defecto para la zona de disponibilidad
 }
 
 variable "name" {

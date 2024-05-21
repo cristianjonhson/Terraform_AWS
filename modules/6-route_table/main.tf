a3b6792ebb76f79e8d241a81ed6ad956838c46d1
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-05-20 23:44:31 -0400

    Creacion del modulo recurso Route table

--- modules/6-route_table/main.tf
+++ modules/6-route_table/main.tf
@@ -1,12 +1,12 @@
-# Crear una instancia EC2
-resource "aws_instance" "free_tier_instance" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.free_tier_subnet.id
-  associate_public_ip_address = true  # Asignar una dirección IPv4 pública a la instancia
-  vpc_security_group_ids      = [aws_security_group.free_tier_sg.id]
+resource "aws_route_table" "free_tier_route_table" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = var.all_traffic_cidr
+    gateway_id = var.igw_id
+  }
 
   tags = {
-    Name = "free-tier-instance"
+    Name = var.name
   }
 }

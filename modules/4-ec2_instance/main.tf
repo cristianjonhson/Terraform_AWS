11e0f59bcd6151ee4e19affb9fa016f1a5db8a90
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-05-20 23:38:37 -0400

    usar la variable name, id_subnet y del Security Groups en el recurso Ec2

--- modules/4-ec2_instance/main.tf
+++ modules/4-ec2_instance/main.tf
@@ -2,11 +2,11 @@
 resource "aws_instance" "free_tier_instance" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.free_tier_subnet.id
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
   associate_public_ip_address = true  # Asignar una dirección IPv4 pública a la instancia
-  vpc_security_group_ids      = [aws_security_group.free_tier_sg.id]
 
   tags = {
-    Name = "free-tier-instance"
+    Name = var.name
   }
 }

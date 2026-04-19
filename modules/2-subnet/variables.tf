b123886e63991ff4d851f71a9cf767bbf98a83bd
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-19 12:17:56 -0400

    feat: traducir descripciones de variables en el módulo de subred

--- modules/2-subnet/variables.tf
+++ modules/2-subnet/variables.tf
@@ -1,19 +1,19 @@
 variable "vpc_id" {
-  description = "ID of the VPC"
+  description = "ID de la VPC"
   type        = string
 }
 
 # Variable para el CIDR block de la subred pública
 variable "subnet_cidr" {
-  description = "CIDR block for the public subnet" # Descripción de la variable
+  description = "Bloque CIDR de la subred pública" # Descripción de la variable
 }
 
 # Variable para la zona de disponibilidad de la subred
 variable "availability_zone" {
-  description = "Availability zone for the subnet" # Descripción de la variable
+  description = "Zona de disponibilidad para la subred" # Descripción de la variable
 }
 
 variable "name" {
-  description = "Name for the subnet"
+  description = "Nombre para la subred"
   type        = string
 }
